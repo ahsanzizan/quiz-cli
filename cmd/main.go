d096223128728fa20bcd699765a229e0d1b9208a
Ahsan <ahsanaz461@gmail.com>
2025-07-10 21:55:09 +0700

    Finish core quiz functionalities (TODO: Add time tracking)

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Question struct {
@@ -53,19 +54,65 @@ func parseQuestionsConfig() ([]Question, error) {
 	return questions, nil
 }
 
+func findAnswerByKey(options []Answer, key string) (Answer, bool) {
+	for _, a := range options {
+		if strings.EqualFold(a.Key, key) {
+			return a, true
+		}
+	}
+
+	return Answer{}, false
+}
+
 func main() {
 	questions, err := parseQuestionsConfig()
 	if err != nil {
 		panic("Failed to open the questions config file.")
 	}
 
+	fmt.Println("Starting a quiz session...")
+	fmt.Println()
+
+	correctAnswers := 0
+
 	for index, question := range questions {
-		fmt.Printf("%v. %s", index, question.Question)
-		// Print the answers
-		// Prompt the user to answer with the Answer Key
-		// If the key does not exist, then print "Your answer is invalid, please answer properly this time." -> Re-prompt (While true loop)
-		// If the key exist, then check if the key is a correct answer
-		// If it is a correct answer, then modify a Score value somewhere
-		// Continue to next question
+		fmt.Printf("%d.  %s\n", index+1, question.Question)
+
+		// Print the answers option
+		for _, answer := range question.Answers {
+			fmt.Printf("    %s. %s\n", answer.Key, answer.Value)
+		}
+
+		var userAnswer string
+
+		for {
+			// Prompt the user to answer with the Answer Key
+			fmt.Print("Enter your answer: ")
+			fmt.Scan(&userAnswer)
+
+			// If the key does not exist, then print "Your answer is invalid, please answer properly this time." -> Re-prompt (While true loop)
+			answer, isFound := findAnswerByKey(question.Answers, userAnswer)
+			if !isFound {
+				// Reloop current question
+				fmt.Printf("Your answer (%s) is not recognized as a valid answer to the question.", userAnswer)
+				continue
+			}
+
+			// If the key exist, then check if the key is a correct answer
+			if answer.IsCorrect {
+				// If it is a correct answer, then modify a Score value somewhere
+				correctAnswers++
+			}
+
+			break
+		}
+
+		if index != len(questions)-1 {
+			fmt.Println("Proceeding to the next question.")
+		}
+
+		fmt.Println()
 	}
+
+	fmt.Printf("\nCongratulations! You've just finished the quiz.\nYour total score: %d/%d", correctAnswers, len(questions))
 }
